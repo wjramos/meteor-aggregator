f040beac49a9c6f816f03eb3eb694df5fcbba7ed
Jordan Ramos <me@jordanramos.com>
2016-05-07 14:53:08 -0700

    initial

---
+++ client/main.jsx
@@ -0,0 +1,15 @@
+import React    from 'react';
+import {Meteor} from 'meteor/meteor';
+import {render} from 'react-dom';
+
+import '../imports/startup/accounts-config';
+import App from '../imports/ui/App.jsx';
+
+Meteor.startup(
+  () => {
+    render(
+      <App/>,
+      document.getElementById('render-target')
+    );
+  }
+);
