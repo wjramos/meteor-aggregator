fd56d67af8e3e732c07bc4441e75bcd4ff7a466a
Jordan Ramos <jramos@rei.com>
2016-05-19 00:34:16 -0700

    target blank for logo

--- client/views/Header.jsx
+++ client/views/Header.jsx
@@ -6,29 +6,31 @@ import classnames from 'classnames';
 export default class Header extends Component {
   render ( ) {
     return (
-      <header className = "container-fluid">
-        <div className = "row">
-          <div className = "col-xs-12 text-center">
+      <header className = 'container-fluid'>
+        <div className = 'row'>
+          <div className = 'col-xs-12 text-center'>
             {/* TODO: Make headings authorable */}
-            <h1 className = "heading-announce">United Outside</h1>
-            <section className = "row">
-              <div className = "col-xs-12 col-sm-8 col-sm-offset-2">
-                <h3 className = "text-uppercase">Scelerisque consectetur consequat porta aenean in taciti phasellus congue facilisi lacus nascetur fusce...</h3>
+            <h1 className = 'heading-announce'>United Outside</h1>
+            <section className = 'row'>
+              <div className = 'col-xs-12 col-sm-8 col-sm-offset-2'>
+                <h3 className = 'text-uppercase'>Scelerisque consectetur consequat porta aenean in taciti phasellus congue facilisi lacus nascetur fusce...</h3>
               </div>
             </section>
-            <section className = "row">
-              <div className = "col-xs-2 col-xs-offset-5 vertical-pull-xl">
-                <a href = "https://rei.com">
-                  <img src = "/img/co-op.svg" alt = "Sponsored by REI Co-op&trade;"/>
+            <section className = 'row'>
+              <div className = 'col-xs-2 col-xs-offset-5 vertical-pull-xl'>
+                <a href = 'https://rei.com'
+                   target = '_blank'>
+                  <img src = '/img/co-op.svg'
+                       alt = 'Sponsored by REI Co-op&trade;'/>
                 </a>
               </div>
             </section>
 
-            {/* TODO: Toggle "Past Events" visibility -- Only visible to admin */}
+            {/* TODO: Toggle 'Past Events' visibility -- Only visible to admin */}
             {/*
-            <label className = "hide-completed">
+            <label className = 'hide-completed'>
               <input
-                type    = "checkbox"
+                type    = 'checkbox'
                 checked = { this.state.hideCompleted }
                 onClick = { this.toggleHideCompleted.bind( this ) }
                 readOnly
@@ -42,13 +44,13 @@ export default class Header extends Component {
             {/* TODO: Display admin status */}
             {/*
             { this.props.currentUser ?
-              <form className = "new-tile"
+              <form className = 'new-tile'
                     onSubmit  = { this.handleSubmit.bind( this ) }
                 >
                 <input
-                  type        = "text"
-                  ref         = "textInput"
-                  placeholder = "Type to add new tiles"
+                  type        = 'text'
+                  ref         = 'textInput'
+                  placeholder = 'Type to add new tiles'
                 />
               </form> : ''
             */}
