14cec9a9f5171b276bfe9b37d27db62b4c1d648b
Jordan Ramos <jramos@rei.com>
2016-05-23 15:26:12 -0700

    better data-get logging

--- server/poll.js
+++ server/poll.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Tiles } from '../lib/collections';
-
 import {
   WP,       WP_QUERY,
   CURALATE, CURALATE_QUERY,
@@ -11,23 +10,40 @@ const POLL_INTERVAL = 300000; // 5 min
 
 function poll ( ) {
   const curEntries = Tiles.find( ).fetch( );
+
   console.log( `\n---------------- POLLING ( ${ ( POLL_INTERVAL / 1000 / 60 ).toFixed( 1 ) } minute interval )----------------\n`,
                `Initial Tiles collection size: ${ curEntries.length } tiles` );
 
-  const social = Meteor.call( 'getData', CURALATE, CURALATE_QUERY );
-  const events = Meteor.call( 'getData', EVENTS,   EVENTS_QUERY );
-  const posts  = Meteor.call( 'getData', WP,       WP_QUERY );
+  let newItems = 0;
 
-  Meteor.call( 'upsert', social.items,  'mapSocial' );
-  Meteor.call( 'upsert', posts.posts,   'mapPost' );
-  Meteor.call( 'upsert', events.events, 'mapEvent' );
+  function getEvents ( ) {
+    const events = Meteor.call( 'getData', EVENTS, EVENTS_QUERY );
+    Meteor.call( 'upsert', events.events, 'mapEvent' );
+    return events.events || [];
+  }
 
-  const newItems = social.items.length || 0 + posts.posts.length || 0 + events.events.length || 0;
+  function getPosts ( ) {
+    const posts  = Meteor.call( 'getData', WP, WP_QUERY );
+    Meteor.call( 'upsert', posts.posts, 'mapPost' );
+    return posts.posts || [];
+  }
+
+  function getSocial ( ) {
+    const social = Meteor.call( 'getData', CURALATE, CURALATE_QUERY );
+    Meteor.call( 'upsert', social.items, 'mapSocial' );
+    return social.items || [];
+  }
+
+  let events = getEvents();
+  let posts  = getPosts();
+  let social = getSocial();
+
   const updatedEntries = Tiles.find( ).fetch( );
-  const updatedCount = updatedEntries.length - curEntries.length;
+  const retrievedCount = events.length + posts.length + social.length;
+  const difference     = updatedEntries.length - curEntries.length;
   console.log(
-    `\nNew Tiles collection size:
-     ${ updatedEntries.length } tiles ( ${ updatedCount } new, ${ updatedEntries.length - updatedCount } updated )\n`
+    `\n\nNew Tiles collection size:
+     ${ updatedEntries.length } tiles ( ${ difference } new, ${ retrievedCount - difference } updated )\n`
   );
 }
 
