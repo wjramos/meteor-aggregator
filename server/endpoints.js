347e1db3661ca7e5fea717dbae299c5b34b1f3ac
Jordan Ramos <jramos@rei.com>
2016-06-13 11:04:56 -0700

    Improve queries

--- server/endpoints.js
+++ server/endpoints.js
@@ -5,26 +5,35 @@ export const CURALATE_QUERY = {
   limit: 10000
 };
 
+/* API Documentation: https://wordpress.org/plugins/json-api/other_notes/ */
 export const WP       = 'http://brightestyoungthings.com/api/get_recent_posts';
 export const WP_QUERY = {
-  limit: 10000,
-  // tag:   'rei'
+  count:     10000,
+  tag:       'rei',
+  include:   [ 'date', 'title', 'excerpt', 'attachments' ],
+  order_by:  'date',//'modified'
+  order:     'desc',
+  read_more: '...',
+  // date_format: '',
 };
 
-export const EVENTS = 'https://rei.com/rest/events/nearby';
+export const EVENTS = 'https://future.rei.com/rest/events/nearby';
 export const EVENTS_QUERY = {
   limit:         10000,
   sortBy:        'date',
-  sortDirection: 'asc',
+  sortDirection: 'desc',
   offset:        0,
   distance:      100,
   location:      20500,
-  // sa:            'United DC',
+  sa:            'United DC',
   // ca:            'Women Only'
 };
 
 export const PROGRAMS = {
-    climbing:          21,
-    cycling:           22,
-    'Outdoor Fitness': 83
+    'Climbing':            21,
+    'Cycling':             22,
+    'Outdoor Fitness':     83,
+    'Hiking & Camping':    25,
+    'Paddling' :           24,
+    'Outdoor Photography': 41
 };
