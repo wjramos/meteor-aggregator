75db0e5fb76430c8bfe4cbf60633ee76b9d900f5
Jordan Ramos <jramos@rei.com>
2016-06-16 10:01:32 -0700

    Generate stars

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,26 +1,20 @@
 import React, { Component, PropTypes } from "react";
 import { STAR_SRC, TITLE, LEAD, LOGO_HREF, LOGO_SRC, LOGO_ALT } from '../../imports/content'
 
+const Star = ( ) => (
+  <div className = "col-xs-3 col-md-4">
+    <div className = "well-sm">
+      <img className = "img-responsive" src = { STAR_SRC } />
+    </div>
+  </div>
+);
+
 export default Header = ( ) => (
   <header className = "container-fluid">
     <div className = "row row-flex">
       <div className = "col-xs-4 col-sm-3 col-md-2">
         <div className = "row row-flex nowrap">
-          <div className = "col-sm-4">
-            <div className = "well-sm">
-              <img className = "img-responsive" src = { STAR_SRC } />
-            </div>
-          </div>
-          <div className = "col-sm-4">
-            <div className = "well-sm">
-              <img className = "img-responsive" src = { STAR_SRC } />
-            </div>
-          </div>
-          <div className = "col-sm-4">
-            <div className = "well-sm">
-              <img className = "img-responsive" src = { STAR_SRC } />
-            </div>
-          </div>
+          { [ ...Array( 3 ) ].map( ( ) => ( <Star /> ) ) }
         </div>
       </div>
       <div className = "col-xs-12">
